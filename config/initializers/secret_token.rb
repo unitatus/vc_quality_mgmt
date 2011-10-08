b3bccf3d7a33738dacecf5356d760e9ac5746166
David Zabinski <dave@thevisiblecloset.com>
2011-10-07 23:12:54 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VcQualityMgmt::Application.config.secret_token = '1bbda41d787853d53411b088ebc65157089ac07d1aba4c221524389caf006963f3c890d0a8834c3efb9687089b24b376beb002b206ddc19b575365802615713d'
